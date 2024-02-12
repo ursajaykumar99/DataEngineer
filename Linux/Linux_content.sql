85b46704e5633d8691365285f5d325bc0554ba41
Ajay Kumar <Ajay Kumar>
2024-02-12 07:17:51 +0530

    Linux_content Updated

--- Linux/Linux_content.sql
+++ Linux/Linux_content.sql
@@ -9,19 +9,19 @@ Linux File architecture:
 
 /		:	root Main folder
 bin		:	binary files ( Common Executable commands)
-boot	:	system booting files
-sbin	:	super user executable 	--commands like adding newusers,deleting users.
-root	:	root superuser folder
+boot	        :	system booting files
+sbin	        :	super user executable 	--commands like adding newusers,deleting users.
+root	        :	root superuser folder
 dev		:	Device related information
-home	: 	users personal space
+home	        : 	users personal space
 etc		:	all configuration files
 opt		:	third party apps related information will store here.
-proc	:	process related information
+proc	        :	process related information
 sys		:	systemInformation
 usr		:	User related folder will create all folders under every USER
 var		:	Variable folder ( logs, backups )
 tmp		:	temporary files
-media	:
+media	        :
 mnt		:
 
 ------------------------------------------------------------------------------------------------------------
@@ -31,12 +31,12 @@ Basic Commands:
 
 whoami					:	will gives the who are you.
 uname					:	Gives the what it is.
-pwd						:	Present working directory 	/* Gives the Current path. */
+pwd					:	Present working directory 	/* Gives the Current path. */
 
-ls	[Options] [fd]		:	list side	/* Gives the all the files list in side by side */
+ls	[Options] [fd]			:	list side	/* Gives the all the files list in side by side */
 ls -l					:	long List   /* GIves the long lost for the all files in that folder */
-stat <filename>			:	/* For Property verification */
-cd <foldername>			:	change ditectory /* To change your directory location for forward this command will help */	
+stat <filename>				:	/* For Property verification */
+cd <foldername>				:	change ditectory /* To change your directory location for forward this command will help */	
 cd ..					:	/* For one step back position */
 cd <path>				:	/* To move particular speicifed path */
 cd ~					: 	To Move Home Folder
@@ -47,20 +47,26 @@ Output > Filename		:	To create a file with the output of the command
 touch <filename>		:	/* For creating a new file */
 cat <filename>			:	/* For displayig what is inside of the file */
 tac <filename>			:	/* For displaying a file in reverse order */
-more <filename>			:	/* For displaying a large files */
 nano <filename>			:	/* For Editing that file */
 vi <filename>			:	/* For Editing that file */
 
 rm <filename>			:	Remove			/* For deleting a existing file */
-mv <filename> <path>	:	Move    		/* Moving the file to the particualr path */
-mv <filename> <newname>	:	Rename			/* For Renaming file*/
-cp <filename> <path>	:	Copy			/* Copy the file to the another path */	
-cp <filename> <filename>:	Copy			/* Copy the value in the file to the another file */
+mv <filename> <path>		:	Move    		/* Moving the file to the particualr path */
+mv <filename> <newname>		:	Rename			/* For Renaming file*/
+cp <filename> <path>		:	Copy			/* Copy the file to the another path */	
+cp <filename> <filename>	:	Copy			/* Copy the value in the file to the another file */
 
 head <filename>			:
 tail <filename>			:
 more <filename>			:
-less <filename>			:
+
+less <filename>			:	/* Useful to view the Big file & to find the patterns in it */
+	arrowup			: 	To move page one line by up*
+	arrowdown		:	To Move page one line down
+	G			:	Come to end of the file
+	g			: 	Come to Start of the file
+	/pattern  + n		:	Search Pattern from top to down
+	?Pattern  + n 		: 	Search Pattern from down to up
 
 mkdir <foldername>		:	Make directory 	/* For creating a new folder */
 rmdir <foldername>		:	Remove directory /* For Deleting the folder if it's empty */
@@ -91,12 +97,12 @@ pipe(|): to write two or more commands at a time we can use pipe
 
 
 ------------------------------------------------------------------------------------------------------------
-date	:
+date		:
 cal		:
-echo	:
-expr	:
+echo		:
+expr		:
 ``		:
 ;		:
-alias	:
-unalias	:
+alias		:
+unalias		:
 wc		:
